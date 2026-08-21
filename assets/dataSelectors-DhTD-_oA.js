c65873a8b72715a04e6983465c9072aab794a22b
Fatgezim Bela <43324078+Fatgezimb@users.noreply.github.com>
2026-08-21 03:51:02 -0400

    chore: sync founding access handoff

--- assets/dataSelectors-DhTD-_oA.js
+++
@@ -1,1 +0,0 @@
-import{k as r}from"./index-Bnmb8KvN.js";import"./practiceQuestions-CCRDNQTt.js";function n(t,e=r){return e.filter(o=>o.section===t)}export{n as g};
---
+++ assets/purchaseNavigation-otqbE6ep.js
@@ -0,0 +1,1 @@
+function t(r){return r==="monthly"||r==="annual"?r:null}function a(r){return`/contact?${new URLSearchParams({topic:"Purchase interest",plan:r,from:"pricing"}).toString()}`}export{a,t as p};
